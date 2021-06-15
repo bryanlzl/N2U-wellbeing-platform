c88775204d05868c066ed59da0b43c174074c408
Bryan <58539426+bryanlzl@users.noreply.github.com>
2021-06-16 00:44:40 +0800

    Update n2usentiment.py

--- n2usentiment.py
+++ n2usentiment.py
@@ -8,24 +8,22 @@ import matplotlib.pyplot as plt
 from collections import Counter
 
 def sentimentanalysis(text):
-    text=text.lower()
-    text=text.replace("\n","")
-    text=text.translate(str.maketrans("","",string.punctuation))
-    tokenize_words=word_tokenize(text)
-    clean_words=[]
+    cleaned_list, emotion_list = [], []
+    text = text.lower()
+    text = text.replace("\n", "")
+    text = text.translate(str.maketrans("", "", string.punctuation))
+    tokenize_words = word_tokenize(text)
     for i in tokenize_words:
         if i not in stopwords.words("english"):
-            clean_words.append(i)
-    emotions=[]
-    with open("emotion.txt","r") as file:
-        for i in file:
-            text1=i.replace("\n","")
-            text1=text1.strip()
-            text1=text1.replace(" ","")
-            text1=text1.replace(",","")
-            text1=text1.replace("'","")
-            word,emotion=text1.split(":")
+            cleaned_list.append(i)
+    with open("emotion.txt", "r") as file:
+        for words in file:
+            keyword = words.replace("\n", "")
+            keyword = keyword.strip()
+            keyword = keyword.replace(" ", "")
+            keyword = keyword.replace(",", "")
+            keyword = keyword.replace("'", "")
+            word, emotion = keyword.split(":")
             if word in clean_words:
-                emotions.append(emotion)
-
+                emotion_list.append(emotion)
     return [SentimentIntensityAnalyzer().polarity_scores(text), Counter(emotions)]
